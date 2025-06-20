28fc6871793c47f4971753edee2f317b8df657f1
Rajan Prasad <tumhara-github-email@example.com>
2025-06-20 17:32:17 +0530

    Initial frontend push

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,10 +1,14 @@
-// import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api/v1/";
+
+
+
+
+// // Located at: C:/Users/rajan/Downloads/Hungry_food/Hungry_food/Hungry_food/reastaurant/client/src/Helpers/axiosInstance.js
+// import axios from 'axios';
 
 // const axiosInstance = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true,
+//     baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1', // Using import.meta.env
+//     withCredentials: true,
 // });
 
 // export default axiosInstance;
@@ -15,28 +19,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
-// Located at: C:/Users/rajan/Downloads/Hungry_food/Hungry_food/Hungry_food/reastaurant/client/src/Helpers/axiosInstance.js
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1', // Using import.meta.env
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_BACKEND_URL, // always from .env
+  withCredentials: true,
 });
 
 export default axiosInstance;
-
-
-
-
-
-
-
