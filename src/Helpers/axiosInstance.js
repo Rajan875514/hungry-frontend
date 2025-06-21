7b8322db340c95563909d5ee7219d448a6ddeb8e
Rajan Prasad <tumhara-github-email@example.com>
2025-06-21 15:33:15 +0530

    fix: backend API connected and working

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,28 +1,7 @@
-
-
-
-
-
-// // Located at: C:/Users/rajan/Downloads/Hungry_food/Hungry_food/Hungry_food/reastaurant/client/src/Helpers/axiosInstance.js
-// import axios from 'axios';
-
-// const axiosInstance = axios.create({
-//     baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api/v1', // Using import.meta.env
-//     withCredentials: true,
-// });
-
-// export default axiosInstance;
-
-
-
-
-
-
-
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // always from .env
+  baseURL: import.meta.env.VITE_BACKEND_URL, // pulls from .env
   withCredentials: true,
 });
 
